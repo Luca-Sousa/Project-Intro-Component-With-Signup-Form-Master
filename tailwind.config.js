bc51900af15aa6bdfec0dcb6793fc7b409a20327
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-04 18:01:25 -0300

    Update Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: ['./index.html', './src/*.css'],
   theme: {
     extend: {
+      colors: {
+        'col-R': 'hsl(0, 100%, 74%) ',
+        'col-G': 'hsl(154, 59%, 51%)',
+        'col-B': 'hsl(248, 32%, 49%)',
+        'col-DB': 'hsl(249, 10%, 26%)',
+        'col-GB': 'hsl(246, 25%, 77%)'
+      },
+      fontFamily: {
+        'P': 'Poppins'
+      },
+      backgroundImage: {
+        'Img-D': "url('/images/bg-intro-desktop.png')",
+        'Img-M': "url('/images/bg-intro-mobile.png')"
+      },
+      boxShadow: {
+        'bS': '0px 8px 0px 0px rgba(0,0,0,0.15)',
+        'bSG': '0px 5px 0px 0px rgba(73,174,132,255)'
+      }
     },
   },
   plugins: [],
